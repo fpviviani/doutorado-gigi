1a749290f4ed2b35408f2c17060ae802dc9a0d43
fpviviani <fpviviani@gmail.com>
2026-03-23 15:07:03 -0300

    cleanup: limit initial cleanup to project temp dir

--- Etapas Modelagem/04_cleanup.R
+++ Etapas Modelagem/04_cleanup.R
@@ -8,19 +8,6 @@ cat(paste(rep("=", 70), collapse = ""), "\n\n")
 
 cat("🧹 LIMPEZA INICIAL...\n")
 
-temp_windows <- "C:/Users/giova/AppData/Local/Temp"
-if (dir.exists(temp_windows)) {
-  arquivos <- list.files(temp_windows, full.names = TRUE, recursive = FALSE)
-  if (length(arquivos) > 0) {
-    info <- file.info(arquivos)
-    apenas_arquivos <- rownames(info[!info$isdir, ])
-    if (length(apenas_arquivos) > 0) {
-      cat("   🗑️ Removendo", length(apenas_arquivos), "arquivos\n")
-      unlink(apenas_arquivos, force = TRUE)
-    }
-  }
-}
-
 if (dir.exists(dir_temp)) {
   arquivos_terra <- list.files(dir_temp, full.names = TRUE)
   if (length(arquivos_terra) > 0) {
